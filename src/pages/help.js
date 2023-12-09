7fcf8deca9c7f2b8ba0c13f226719adef15e256e
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-12-09 11:52:12 -0800

    Updates
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- src/pages/help.js
+++ src/pages/help.js
@@ -7,14 +7,23 @@ export default function printerDispatch() {
             <Link href="/" className="hover:text-purple-400">ISB3DPRINTERS: Help Page</Link>
             </div>
             <h1>Help</h1>
+
             <h2>Getting Started</h2>
+
             <h3>Getting a model</h3>
+
             <h3>Slicing the model</h3>
+
             <h3>Uploading the model</h3>
+
             <h3>Getting the model</h3>
+
             <h2>Detailed Slicing Help</h2>
+
             <h2>Detailed Uploading Help</h2>
+
             <h2>Teachers help</h2>
+            
             <p>Go to the help page in the teachers&apos; console page</p>
         </div>
     );
