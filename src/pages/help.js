416b74372f44558d76f0ecb66af646c5e097a28e
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-12-10 09:08:19 -0800

    Fixed and Formatted Teachers Console

--- src/pages/help.js
+++ src/pages/help.js
@@ -4,27 +4,45 @@ export default function printerDispatch() {
     return (
         <div>
             <div className="flex p-2 px-4 text-xl bg-black text-white font-bold justify-between">
-            <Link href="/" className="hover:text-purple-400">ISB3DPRINTERS: Help Page</Link>
+                <Link href="/" className="hover:text-purple-400">ISB3DPRINTERS: Help Page</Link>
             </div>
-            <h1>Help</h1>
 
-            <h2>Getting Started</h2>
-
-            <h3>Getting a model</h3>
-
-            <h3>Slicing the model</h3>
-
-            <h3>Uploading the model</h3>
-
-            <h3>Getting the model</h3>
-
-            <h2>Detailed Slicing Help</h2>
-
-            <h2>Detailed Uploading Help</h2>
-
-            <h2>Teachers help</h2>
-            
-            <p>Go to the help page in the teachers&apos; console page</p>
+            <div>
+                <h1 class="text-5xl font-extrabold dark:text-black">Help</h1>
+            </div>
+            <br></br>
+            <div>
+                <h2 class="text-4xl font-bold dark:text-black">Getting Started</h2>
+            </div>
+            <br></br>
+            <div>
+                <h3 class="text-3xl font-bold dark:text-black">Getting a Model</h3>
+            </div>
+            <br></br>
+            <div>
+                <h3 class="text-3xl font-bold dark:text-black">Slicing the model</h3>
+            </div>
+            <br></br>
+            <div>
+                <h3 class="text-3xl font-bold dark:text-black">Uploading the model</h3>
+            </div>
+            <br></br>
+            <div>
+                <h3 class="text-3xl font-bold dark:text-black">Getting the model</h3>
+            </div>
+            <br></br>
+            <div>
+                <h2 class="text-4xl font-bold dark:text-black">Detailed Slicing Help</h2>
+            </div>
+            <br></br>
+            <div>
+                <h2 class="text-4xl font-bold dark:text-black">Detailed Uploading Help</h2>
+            </div>
+            <br></br>
+            <div>
+                <h2 class="text-4xl font-bold dark:text-black">Teachers help</h2>
+                <p>Go to the help page in the teachers&apos; console page</p>
+            </div>
         </div>
     );
 }
