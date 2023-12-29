132145d4c68731e6f88c0182a92b7ec13ab5d7a5
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-12-28 16:54:52 -0800

    added tailwind css to dispatch page
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- src/pages/printerDispatch.js
+++ src/pages/printerDispatch.js
@@ -4,6 +4,10 @@ import React from 'react';
 
 export default function Page() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const printer1 = baseUrl+'8001';
+  const printer2 = baseurl+'8002'
+  const printer3 = baseurl+'8003'
+  const printer4 = baseurl+'8004'
   return (
     <div>
       <div className="flex p-2 px-4 text-xl bg-black text-white font-bold justify-between">
@@ -12,21 +16,102 @@ export default function Page() {
         </Link>
       </div>
       <h2>Make sure to check each printer for avalibility</h2>
-      <Link href={baseUrl + ':8001'} className="hover:text-purple-400">
-        Printer #1
-      </Link>
+      <p class="text-gray-500 dark:text-gray-400">
+        <a
+          href="#"
+          class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Printer #1
+          <svg
+            class="w-4 h-4 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="{http://www.w3.org/2000/svg}"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </p>
+
       <br></br>
-      <Link href={baseUrl + ':8002'} className="hover:text-purple-400">
-        Printer #2
-      </Link>
+      <p class="text-gray-500 dark:text-gray-400">
+        <a
+          href="#"
+          class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Printer #2
+          <svg
+            class="w-4 h-4 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </p>
       <br></br>
-      <Link href={baseUrl + ':8003'} className="hover:text-purple-400">
-        Printer #3
-      </Link>
+      <p class="text-gray-500 dark:text-gray-400">
+        <a
+          href="#"
+          class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Printer #3
+          <svg
+            class="w-4 h-4 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </p>
       <br></br>
-      <Link href={baseUrl + ':8004'} className="hover:text-purple-400">
-        Printer #4
-      </Link>
+      <p class="text-gray-500 dark:text-gray-400">
+        <a
+          href="#"
+          class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Printer #4
+          <svg
+            class="w-4 h-4 ms-2 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </p>
     </div>
   );
 }
