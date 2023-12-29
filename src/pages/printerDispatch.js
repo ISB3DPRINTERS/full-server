5420140323ba333486c5724d8e06e11ca3fc6d49
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-12-29 11:47:58 -0800

    Added Licensing & Code of Conduct

--- src/pages/printerDispatch.js
+++ src/pages/printerDispatch.js
@@ -1,13 +1,28 @@
+// Copyright 2023 Ravinder Olivier Singh Dadiala
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use server';
+/* eslint-disable react/no-unknown-property */
 import Link from 'next/link';
 import React from 'react';
 
 export default function Page() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const printer1 = baseUrl+'8001';
-  const printer2 = baseurl+'8002'
-  const printer3 = baseurl+'8003'
-  const printer4 = baseurl+'8004'
+  const printer1 = baseUrl+':8001';
+  const printer2 = baseUrl+':8002'
+  const printer3 = baseUrl+':8003'
+  const printer4 = baseUrl+':8004'
   return (
     <div>
       <div className="flex p-2 px-4 text-xl bg-black text-white font-bold justify-between">
@@ -18,7 +33,7 @@ export default function Page() {
       <h2>Make sure to check each printer for avalibility</h2>
       <p class="text-gray-500 dark:text-gray-400">
         <a
-          href="#"
+          href={printer1}
           class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
         >
           Printer #1
@@ -43,7 +58,7 @@ export default function Page() {
       <br></br>
       <p class="text-gray-500 dark:text-gray-400">
         <a
-          href="#"
+          href={printer2}
           class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
         >
           Printer #2
@@ -67,7 +82,7 @@ export default function Page() {
       <br></br>
       <p class="text-gray-500 dark:text-gray-400">
         <a
-          href="#"
+          href={printer3}
           class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
         >
           Printer #3
@@ -91,7 +106,7 @@ export default function Page() {
       <br></br>
       <p class="text-gray-500 dark:text-gray-400">
         <a
-          href="#"
+          href={printer4}
           class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
         >
           Printer #4
