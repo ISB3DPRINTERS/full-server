f2ba623554be86c56af51dfb8a0fac8e87d39199
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2024-01-14 10:12:14 -0800

    Got env variables working

--- src/lib/supabase.mjs
+++ src/lib/supabase.mjs
@@ -12,10 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  'https://servksydavsonantnpox.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcnZrc3lkYXZzb25hbnRucG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NzQ2NjYsImV4cCI6MjAwOTQ1MDY2Nn0.nzbHw3XU4qVouLYJRW0yuVNt89qNESX4tV8m606dd_A'
-);
+var supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+var supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
