de703afd6c858f62e9e768d68b53940cef10fbbb
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2024-01-15 09:02:21 -0800

    Call new function

--- src/pages/api/newinfo.js
+++ src/pages/api/newinfo.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { keychanger } from '../../lib/newinfo.js';
+import keychanger from '../../lib/newinfo.js';
 import * as fs from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
