e863da6ba16e1817a56093669769891d170e1ef6
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2024-01-17 10:23:13 -0800

    Using Next link now

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,20 +97,20 @@ export default function Home() {
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="/about" className="hover:underline me-4 md:me-6">
+              <Link href="/about" className="hover:underline me-4 md:me-6">
                 {' '}
                 About
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/privacypolicy" className="hover:underline me-4 md:me-6">
+              <Link href="/privacypolicy" className="hover:underline me-4 md:me-6">
                 Privacy Policy
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/license" className="hover:underline me-4 md:me-6">
+              <Link href="/license" className="hover:underline me-4 md:me-6">
                 License
-              </a>
+              </Link>
             </li>
             <li>
               <a
