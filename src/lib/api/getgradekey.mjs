1ad9e520da00fee4f81ed9bee6608235b2020473
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2024-01-17 10:56:57 -0800

    Fix

--- src/lib/api/getgradekey.mjs
+++ src/lib/api/getgradekey.mjs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(
   'https://servksydavsonantnpox.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlcnZrc3lkYXZzb25hbnRucG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NzQ2NjYsImV4cCI6MjAwOTQ1MDY2Nn0.nzbHw3XU4qVouLYJRW0yuVNt89qNESX4tV8m606dd_A'
