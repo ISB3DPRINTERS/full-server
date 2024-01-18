46812227d4ddbedc2d6f72efd579dcb3c19cf5e9
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2024-01-18 08:44:16 -0800

    Links?
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,25 +61,25 @@ export default function Home() {
         </div>
       </div>
       <div className="flex justify-center">
-        <Link
-          href="/help"
-        >
-          <div> <a> Help </a>
-          <svg
-            className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
+        <Link href="/help">
+          <div>
+            {' '}
+            <a> Help </a>
+            <svg
+              className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
           </div>
         </Link>
       </div>
@@ -87,16 +87,28 @@ export default function Home() {
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{' '}
-            <Link href='https://github.com/ravinder-Olivier'> <a>Ravinder Olivier Singh Dadiala</a> </Link>
+            <Link href="https://github.com/ravinder-Olivier">
+              {' '}
+              <a>Ravinder Olivier Singh Dadiala</a>{' '}
+            </Link>
             . All Rights Reserved.&nbsp;
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <Link href='/about'> <a>About</a> </Link>
-            <Link href='/privacypolicy'> <a>Privacy Policy</a> </Link>
-              <Link href="/license">
-                <a>License</a>
-              </Link>
-              <Link href='mailto:ravinder-Olivier@outlook.com'> <a>Contact</a> </Link>
+            <Link href="/about">
+              {' '}
+              <a>About</a>{' '}
+            </Link>
+            <Link href="/privacypolicy">
+              {' '}
+              <a>Privacy Policy</a>{' '}
+            </Link>
+            <Link href="/license">
+              <a>License</a>
+            </Link>
+            <Link href="mailto:ravinder-Olivier@outlook.com">
+              {' '}
+              <a>Contact</a>{' '}
+            </Link>
           </ul>
         </div>
       </footer>
