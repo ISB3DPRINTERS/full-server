c4acc0c3855ae32ca7ed8dbf4518bcee8eb5b354
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2024-01-17 20:20:55 -0800

    e

--- src/pages/api/newinfo.js
+++ src/pages/api/newinfo.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 import keychanger from '../../lib/newinfo.js';
 import * as fs from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
