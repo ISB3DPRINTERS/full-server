da64f67a83665fe327567c5353052f73c33dd003
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2024-07-27 16:01:44 -0700

    udpaets
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 The all new, full codebase in a single app!
 
-
 ## Demo Slides:
 
 [Google Slides](https://docs.google.com/presentation/d/1ccHkCeZ2_XfU9klWb3ZJL7IhHsj3IdL6RTlJmsM3UDI/edit#slide=id.g4dfce81f19_0_45)
 
-This slideshow can be edited for use in your own educational insitution. 
+This slideshow can be edited for use in your own educational insitution.
--- src/pages/help.js
+++ src/pages/help.js
@@ -18,58 +18,15 @@ import React from 'react';
 export default function help() {
   return (
     <div>
-      <div className="flex p-2 px-4 text-xl bg-black text-white font-bold justify-between">
-        <Link href="/" className="hover:text-purple-400">
-          ISB3DPRINTERS: Help Page
-        </Link>
-      </div>
+        <div className="flex p-2 px-4 text-xl bg-black text-white font-bold justify-between">
+            <Link href="/" className="hover:text-purple-400">
+                ISB3DPRINTERS: Help Page
+            </Link>
+        </div>
+            <a href={'https://docs.google.com/presentation/d/1ccHkCeZ2_XfU9klWb3ZJL7IhHsj3IdL6RTlJmsM3UDI/edit#slide=id.g26794161b6b_0_265'}>
+                Presentation for help (click)
+            </a>
 
-      <div>
-        <h1 className="text-5xl font-extrabold dark:text-black">Help</h1>
-      </div>
-      <br></br>
-      <div>
-        <h2 className="text-4xl font-bold dark:text-black">Getting Started</h2>
-      </div>
-      <br></br>
-      <div>
-        <h3 className="text-3xl font-bold dark:text-black">Getting a Model</h3>
-      </div>
-      <br></br>
-      <div>
-        <h3 className="text-3xl font-bold dark:text-black">
-          Slicing the model
-        </h3>
-      </div>
-      <br></br>
-      <div>
-        <h3 className="text-3xl font-bold dark:text-black">
-          Uploading the model
-        </h3>
-      </div>
-      <br></br>
-      <div>
-        <h3 className="text-3xl font-bold dark:text-black">
-          Getting the model
-        </h3>
-      </div>
-      <br></br>
-      <div>
-        <h2 className="text-4xl font-bold dark:text-black">
-          Detailed Slicing Help
-        </h2>
-      </div>
-      <br></br>
-      <div>
-        <h2 className="text-4xl font-bold dark:text-black">
-          Detailed Uploading Help
-        </h2>
-      </div>
-      <br></br>
-      <div>
-        <h2 className="text-4xl font-bold dark:text-black">Teachers help</h2>
-        <p>Go to the help page in the teachers&apos; console page</p>
-      </div>
     </div>
-  );
-}
+        );
+        }
